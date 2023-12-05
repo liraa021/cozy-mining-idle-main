b0ab77475b15d6134eb7f6b4136d9c8c2a16b465
Rodrigo Lira <rodrigolira94@gmail.com>
2023-12-04 21:03:36 -0300

    [NEW] Include Things

--- js/script.js
+++ js/script.js
@@ -1,25 +1,86 @@
-// Pega o modal
+// User interface elements
 var modal = document.getElementById("myModal");
-
-// Pega o botão que abre o modal
 var btn = document.getElementById("gear");
-
-// Pega o elemento <span> que fecha o modal
 var span = document.getElementsByClassName("close")[0];
+var volumeSlider = document.getElementById("volumeSlider");
+var soundIcon = document.querySelector(".sound");
+var fullscreenIcon = document.querySelector(".fullscreen");
+var coinNumber = document.querySelector(".coin-number");
 
-// Quando o usuário clica no botão, abre o modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// Initialize coin count
+var coins = 0;
+coinNumber.textContent = coins;
 
-// Quando o usuário clica em <span> (x), fecha o modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// Coin control function
+function increaseCoins() {
+  coins++;
+  coinNumber.textContent = coins;
+}
 
-// Quando o usuário clica fora do modal, fecha ele
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// Volume control functions
+function increaseVolume() {
+  if (volumeSlider.value < 100) {
+    volumeSlider.value++;
   }
-};
+}
+
+function decreaseVolume() {
+  if (volumeSlider.value > 0) {
+    volumeSlider.value--;
+  }
+}
+
+// Sound control function
+function toggleSound() {
+  if (volumeSlider.value != 0) {
+    volumeSlider.value = 0;
+  } else {
+    volumeSlider.value = 50;
+  }
+}
+
+// Modal control functions
+function openModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function closeModalIfClickedOutside(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+}
+
+// Fullscreen control function
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
+// Event listeners
+btn.onclick = openModal;
+span.onclick = closeModal;
+window.onclick = closeModalIfClickedOutside;
+soundIcon.onclick = toggleSound;
+fullscreenIcon.onclick = toggleFullScreen;
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "v") {
+    increaseVolume();
+  } else if (event.key === "c") {
+    decreaseVolume();
+  } else if (event.key === "z") {
+    toggleFullScreen();
+  }
+});
+
+// Increase coins every second
+setInterval(increaseCoins, 1000);
